35b57a1b1565b74408e089827c6180e2fe202666
Javier Garea <javier.garea@nomasystems.com>
2023-03-29 13:13:31 +0200

    chore: move triq generators to standalone repo (#46)

--- include/ndto_schema.hrl
+++ include/ndto_schema.hrl
@@ -11,11 +11,25 @@
 %% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 %% See the License for the specific language governing permissions and
 %% limitations under the License
--ifndef(ndto_pbt).
--define(ndto_pbt, true).
+-ifndef(ndto_schema).
+-define(ndto_schema, true).
 
--type pbt_tool() :: triq.
--type test_data_generator() :: term().
+%%% MACROS
+-define(BASIC_SCHEMAS, [
+    #{<<"type">> => <<"boolean">>},
+    #{<<"type">> => <<"integer">>},
+    #{<<"type">> => <<"number">>},
+    #{<<"type">> => <<"string">>},
+    #{<<"type">> => <<"array">>},
+    #{<<"type">> => <<"object">>}
+]).
+-define(FORMATS, [
+    <<"base64">>,
+    <<"iso8601-datetime">>
+]).
+% https://www.erlang.org/doc/efficiency_guide/advanced.html
+-define(MAX_INT, 134217728).
+-define(MIN_INT, -134217729).
 
-% -ifndef(ndto_pbt)
+% -ifndef(ndto_schema)
 -endif.
